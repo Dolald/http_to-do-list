077e44363357850d1a718e3728128c4afcc51f89
Dolald <vorobushec13@gmail.com>
2024-05-25 03:06:31 +0300

    list-cycle

---
+++ Рабочий стол/InternetToDoList/server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+	app := fiber.New()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
---
+++ Рабочий стол/Leetcode/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(hasCycle)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	visited_nodes := make(map[*ListNode]bool)
+	current_node := head
+	for current_node != nil {
+		if visited_nodes[current_node] {
+			return true
+		}
+		visited_nodes[current_node] = true
+		current_node = current_node.Next
+	}
+	return false
+}
